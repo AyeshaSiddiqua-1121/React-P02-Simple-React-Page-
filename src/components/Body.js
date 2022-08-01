177c9ffe715d9b36c8bcf3d048afd77ffe949747
Mariam Ali <mali.bese18seecs@seecs.edu.pk>
2022-08-01 11:02:08 -0800

    Simple One Page React

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './assets/styles/style.css'
+
+export default function Body() {
+  return (
+    <main>
+        <h1 className='heading'>Fun facts about React</h1>
+        <ul className='facts'>
+            <li>Name is Ayesha Siddiqua</li>
+            <li>Have completed software engineering degree</li>
+            <li>React is nowadays popular JS framework</li>
+            <li>Thousands of enterprise applications are built using React and React Native</li>
+        </ul>
+    </main>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './assets/styles/style.css'
+export const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+        <img src='logo192.png'></img>
+        <h3 className='logo-text'>ReactFacts</h3>
+        <h4 className='nav-item'>React Course-Project 1</h4>
+        </div>
+        
+    </div>
+  )
+}
